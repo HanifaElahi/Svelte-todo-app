5082d8b78add6040d42c0f7445dd44f15625f80f
Hanifa Elahi <49684080+HanifaElahi@users.noreply.github.com>
2021-01-10 08:54:02 +0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import List from "./components/List.svelte";
+</script>
+
+<style>
+  .container {
+    width: 700px;
+    height: 525px;
+    padding: 120px 500px 120px 500px;
+    background:purple;
+  }
+
+  :global(.container *) {
+    font-family: "Quicksand", sans-serif;
+  }
+</style>
+
+<div class="container">
+  <Header />
+  <List />
+</div>
---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { items } from "../stores";
+
+  $: itemCount = $items.length;
+  $: completedItemCount = $items.filter((item) => item.complete).length;
+</script>
+
+<style>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 25px 25px;
+    color: #eeeeee;
+    font-weight: bold;
+    font-size: 1.5em;
+    background: rgba(0, 0, 0, 0.6);
+  }
+</style>
+
+<div class="header">
+  <div>My To-Do List</div>
+  <span> {completedItemCount}/{itemCount} </span>
+</div>
